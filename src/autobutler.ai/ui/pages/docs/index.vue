9579f783b3b1d2e3d32d9a99e975465d0aef81fd
Brandon Apol <brandonapol@cedarville.edu>
2025-05-31 15:52:08 -0400

    chore: fix md ssg

--- src/autobutler.ai/ui/pages/docs/index.vue
+++ src/autobutler.ai/ui/pages/docs/index.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script setup lang="ts">
-// Redirect to the welcome page which is handled by [...slug].vue
+// Makes sure the markdown pages are rendered before the generation is done
+prerenderRoutes(["/docs/welcome"]);
 await navigateTo("/docs/welcome");
 </script>
