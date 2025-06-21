ca3814582c35032dd386f67ef222edf3eaa24ef8
James Orson <jamesaorson@gmail.com>
2025-06-21 10:50:08 -0700

    ui-rewrite: Move upwards some directories
    Signed-off-by: James Orson <jamesaorson@gmail.com>

---
+++ src/autobutler/cmd/serve.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"autobutler/internal/server"
+
+	"github.com/spf13/cobra"
+)
+
+func Serve() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "serve",
+		Short: "Start the Autobutler server",
+		Long:  `The serve command starts the Autobutler server, allowing you to interact with the Autobutler system through its API.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Starting Autobutler server...")
+			if err := server.StartServer(); err != nil {
+				fmt.Fprintf(os.Stderr, "Error starting server: %v\n", err)
+				os.Exit(1)
+			}
+		},
+	}
+
+	return cmd
+}
