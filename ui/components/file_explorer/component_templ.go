0800dd2b59128526678f4ca6ae30fcbd88ba96c7
James Orson <jamesaorson@gmail.com>
2025-07-08 11:25:28 -0700

    Fix max width on file explorer
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- ui/components/file_explorer/component_templ.go
+++ ui/components/file_explorer/component_templ.go
@@ -34,7 +34,7 @@ func Component(rootDir string, files []fs.FileInfo) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div id=\"file-explorer\" class=\"bg-white dark:bg-gray-900 max-w-2xl mx-auto mt-10 rounded shadow p-6\"><div class=\"flex items-center mb-4\"><h2 class=\"text-2xl font-bold mr-4\">File Explorer</h2>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div id=\"file-explorer\" class=\"bg-white dark:bg-gray-900 max-w-2xl mx-auto mt-10 rounded shadow p-6\"><div class=\"flex items-center mb-4\"><h2 class=\"text-2xl font-bold mr-4 whitespace-nowrap\">File Explorer</h2>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
