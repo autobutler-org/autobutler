de80ea732b46db2bbd843f9ed28647319f0fce3b
Brandon Apol <brandonapol@cedarville.edu>
2025-11-06 23:02:12 -0500

    generate and lint

--- internal/server/ui/components/service_icons/component_templ.go
+++ internal/server/ui/components/service_icons/component_templ.go
@@ -91,7 +91,7 @@ func Component(icons []types.ServiceIcon) templ.Component {
 				var templ_7745c5c3_Var4 string
 				templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(icon.Label)
 				if templ_7745c5c3_Err != nil {
-					return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/server/ui/components/service_icons/component.templ`, Line: 25, Col: 50}
+					return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/server/ui/components/service_icons/component.templ`, Line: 25, Col: 51}
 				}
 				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 				if templ_7745c5c3_Err != nil {
