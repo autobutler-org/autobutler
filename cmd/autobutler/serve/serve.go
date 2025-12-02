aaa394d3b8d4e3c822fa3d23ca1e7b7f64bfffad
James Orson <jamesaorson@gmail.com>
2025-12-01 18:25:45 -0800

    411: asd
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- cmd/autobutler/serve/serve.go
+++ cmd/autobutler/serve/serve.go
@@ -13,7 +13,7 @@ func Cmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "serve",
 		Short: "Start the Autobutler server",
-		Long:  `The serve command starts the Autobutler server, allowing you to interact with the Autobutler system through its serverutil.`,
+		Long:  `The serve command starts the Autobutler server, allowing you to interact with the Autobutler system through its API.`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			fmt.Println("Starting Autobutler server...")
 			deps, err := deputil.DefaultDependencies()
