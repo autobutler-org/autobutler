aa837e4d58482f17b8cc0291b304e1623a102015
James Orson <jamesaorson@gmail.com>
2025-12-20 12:40:05 -0800

    481: Add to router
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import BooksView from '@/views/BooksView.vue'
+import SettingsView from '@/views/SettingsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -48,6 +49,11 @@ const router = createRouter({
       path: '/devices',
       name: 'devices',
       component: () => import('../views/DevicesView.vue'),
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: SettingsView,
     },
   ],
 })
