4c2e72367f022e3911d43f53952a13252b1b6803
James Orson <jamesaorson@gmail.com>
2025-12-20 08:12:49 -0800

    481: Fix icon colors
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- app/src/components/icons/PdfIcon.vue
+++ app/src/components/icons/PdfIcon.vue
@@ -5,7 +5,7 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="icon icon--base"
+    class="icon icon--base icon--pdf"
     fill="none"
     viewBox="0 0 24 24"
     stroke="currentColor"
