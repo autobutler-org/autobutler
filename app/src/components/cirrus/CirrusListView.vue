443174bc2826b1fb31a2bd66cabc32894ccea393
James Orson <jamesaorson@gmail.com>
2025-12-24 08:51:11 -0800

    frontend-refactor: Move SortSwitcherIcon out to its own component
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- app/src/components/cirrus/CirrusListView.vue
+++ app/src/components/cirrus/CirrusListView.vue
@@ -19,30 +19,7 @@
               :title="mixedSorting ? 'Switch to Folders First sorting' : 'Switch to Mixed sorting'"
               @click="toggleMixedSorting"
             >
-              <div class="sort-switcher-icons">
-                <svg class="sort-switcher-arrows" fill="currentColor" viewBox="0 0 24 24">
-                  <path d="M7 8l5-5 5 5z"></path>
-                  <path d="M7 16l5 5 5-5z"></path>
-                </svg>
-                <svg
-                  v-if="!mixedSorting"
-                  class="sort-switcher-folder"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path
-                    d="M2 6a2 2 0 012-2h5l2 2h5a2 2 0 012 2v6a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-                  ></path>
-                </svg>
-                <svg v-else class="sort-switcher-file" fill="currentColor" viewBox="0 0 20 20">
-                  <path
-                    fill-rule="evenodd"
-                    d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </div>
-              <span class="sort-switcher-label">{{ mixedSorting ? 'Mixed' : 'Folders' }}</span>
+              <SortSwitcherIcon :mixed-sorting="mixedSorting" />
             </button>
           </th>
         </tr>
@@ -90,7 +67,7 @@
                 class="device-badge"
                 :title="'Device: ' + file.deviceName"
               >
-              <DeviceBadge :device-name="file.deviceName" />
+                <DeviceBadge :device-name="file.deviceName" />
               </span>
             </td>
             <td class="file-table-cell file-table-size">
@@ -137,6 +114,7 @@ import CirrusListViewSortHeader, {
   type SortDirection,
 } from './CirrusListViewSortHeader.vue'
 import DeviceBadge from '@/components/badges/DeviceBadge.vue'
+import SortSwitcherIcon from '@/components/icons/SortSwitcherIcon.vue'
 
 const props = defineProps<{
   files: CirrusFileNode[]
@@ -445,39 +423,6 @@ const handleContextMenu = (event: MouseEvent, file: CirrusFileNode) => {
     &:hover {
       background-color: $color-gray-700;
     }
-  }
-}
-
-.sort-switcher-icons {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: $spacing-xs;
-  margin-bottom: $spacing-xs;
-  height: 1rem;
-  padding-left: $spacing-xs;
-}
-
-.sort-switcher-arrows,
-.sort-switcher-folder,
-.sort-switcher-file {
-  width: 1rem;
-  height: 1rem;
-  color: $color-gray-500;
-
-  @media (prefers-color-scheme: dark) {
-    color: $color-gray-400;
-  }
-}
-
-.sort-switcher-label {
-  color: $color-gray-500;
-  font-weight: 600;
-  width: 3rem;
-  text-align: center;
-
-  @media (prefers-color-scheme: dark) {
-    color: $color-gray-400;
   }
 }
 
---
+++ app/src/components/icons/SortSwitcherArrowsIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg class="sort-switcher-arrows" fill="currentColor" viewBox="0 0 24 24">
+    <path d="M7 8l5-5 5 5z"></path>
+    <path d="M7 16l5 5 5-5z"></path>
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+.sort-switcher-arrows {
+  width: 1rem;
+  height: 1rem;
+  color: $color-gray-500;
+
+  @media (prefers-color-scheme: dark) {
+    color: $color-gray-400;
+  }
+}
+</style>
