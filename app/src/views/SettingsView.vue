5472c229f09ec9c47344fe852743dd52daa9716b
James Orson <jamesaorson@gmail.com>
2025-12-24 09:42:24 -0800

    frontend-refactor: Remove thanks section
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- app/src/views/SettingsView.vue
+++ app/src/views/SettingsView.vue
@@ -2,25 +2,6 @@
   <LibraryLayout>
     <template #sidebar>
       <LibrarySidebar :sections="sidebarSections" />
-      <div class="settings-sidebar-thanks">
-        <h2>Thanks</h2>
-        <a href="/thanks" class="settings-thanks-link">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            width="20"
-            height="20"
-          >
-            <path
-              d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"
-            ></path>
-          </svg>
-          <span>View Thanks</span>
-        </a>
-      </div>
     </template>
     <template #title>
       <h2 class="library-title">Settings & Metrics</h2>
