927907e4a361734792f839042a34bd44494a5261
James Orson <jamesaorson@gmail.com>
2025-12-24 09:12:41 -0800

    frontend-refactor: Move svg
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- app/src/components/DeviceCard.vue
+++ app/src/components/DeviceCard.vue
@@ -1,19 +1,7 @@
 <template>
   <div class="device-card" @click="goToCirrus">
     <div class="device-card-header">
-      <div class="device-card-icon">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
-          <line x1="8" y1="21" x2="16" y2="21"></line>
-          <line x1="12" y1="17" x2="12" y2="21"></line>
-        </svg>
-      </div>
+      <DeviceCardIcon />
       <div class="device-card-title-section">
         <h3 class="device-card-title">{{ device.name }}</h3>
         <p class="device-card-type">
@@ -35,6 +23,7 @@
 
 <script setup lang="ts">
 import StoragePartition from './StoragePartition.vue'
+import DeviceCardIcon from '@/components/icons/DeviceCardIcon.vue'
 
 defineProps({
   device: {
@@ -65,41 +54,34 @@ const goToCirrus = () => {
   align-items: center;
   gap: 1.5rem;
 }
-.device-card-icon {
-  width: 64px;
-  height: 64px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-shrink: 0;
-}
-.device-card-icon svg {
-  width: 48px;
-  height: 48px;
-  color: white;
-}
+
 .device-card-title-section {
   display: flex;
   flex-direction: column;
   gap: 0.2rem;
 }
+
 .device-card-title {
   font-size: 1.5rem;
   font-weight: 700;
   color: white;
   margin: 0;
 }
+
 .device-card-type {
   font-size: 1rem;
   color: #b3b3b3;
   margin: 0;
 }
+
 .device-card-body {
   margin-top: 0.5rem;
 }
+
 .device-card-footer {
   margin-top: 0.8rem;
 }
+
 .device-card-mount {
   font-size: 1rem;
   color: #e0e0e0;
