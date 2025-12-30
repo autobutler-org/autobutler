56b7581e81dd3952ae987a38286723cdc4024573
James Orson <jamesaorson@gmail.com>
2025-12-29 17:22:29 -0800

    color-pallette: Nice
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- app/src/components/DeviceCard.vue
+++ app/src/components/DeviceCard.vue
@@ -42,9 +42,9 @@ const goToCirrus = () => {
 
 <style lang="scss" scoped>
 .device-card {
-  background: rgba($color-gray-950, 0.12);
+  background: rgba($theme-palette-bg-primary, 0.7);
   border-radius: 18px;
-  box-shadow: 0 2px 12px 0 rgba($color-gray-950, 0.1);
+  box-shadow: 0 2px 12px 0 rgba($theme-palette-bg-nav, 0.15);
   padding: 2.2rem 2.2rem 1.5rem 2.2rem;
   display: flex;
   flex-direction: column;
@@ -69,13 +69,13 @@ const goToCirrus = () => {
 
 .device-card-mount {
   font-size: $theme-font-size-base;
-  color: $color-gray-200;
+  color: $theme-palette-text-secondary;
 }
 
 .device-card-title {
   font-size: $theme-font-size-2xl;
   font-weight: 700;
-  color: $color-gray-50;
+  color: $theme-palette-text-primary;
   margin: 0;
 }
 
@@ -87,7 +87,7 @@ const goToCirrus = () => {
 
 .device-card-type {
   font-size: $theme-font-size-base;
-  color: $color-gray-400;
+  color: $theme-palette-text-muted;
   margin: 0;
 }
 </style>
