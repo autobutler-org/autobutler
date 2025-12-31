7877735386df3401a91b091e45cc6f6427427d25
James Orson <jamesaorson@gmail.com>
2025-12-31 15:00:14 -0800

    usb-support: Support downloadFile across devices
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- internal/server/api/v1/cirrus/download_file.go
+++ internal/server/api/v1/cirrus/download_file.go
@@ -13,53 +13,68 @@ import (
 )
 
 var downloadFileRoute = serverutil.ApiRoute(
-	"GET", "/download/cirrus/*filePath", func(c *gin.Context) *serverutil.Response {
-		filePath := c.Param("filePath")
+       "GET", "/download/cirrus/*filePath", func(c *gin.Context) *serverutil.Response {
+	       filePath := c.Param("filePath")
+	       deviceName := c.Query("device")
 
-		managedDevices, err := cirrusutil.GetManagedDevices()
-		if err != nil {
-			managedDevices = nil
-		}
+	       var managedDevices []cirrusutil.ManagedDevice
+	       if deviceName == "" {
+		       var err error
+		       managedDevices, err = cirrusutil.GetManagedDevices()
+		       if err != nil {
+			       managedDevices = nil
+		       }
+	       } else {
+		       allDevices, err := cirrusutil.GetManagedDevices()
+		       if err == nil {
+			       for _, d := range allDevices {
+				       if d.Name == deviceName {
+					       managedDevices = append(managedDevices, d)
+					       break
+				       }
+			       }
+		       }
+	       }
 
-		result, err := cirrusutil.DownloadFile(cirrusutil.DownloadFileParams{
-			FilePath:       filePath,
-			ManagedDevices: managedDevices,
-		})
+	       result, err := cirrusutil.DownloadFile(cirrusutil.DownloadFileParams{
+		       FilePath:       filePath,
+		       ManagedDevices: managedDevices,
+	       })
 
-		if err != nil {
-			c.Status(http.StatusNotFound)
-			return serverutil.Ok()
-		}
+	       if err != nil {
+		       c.Status(http.StatusNotFound)
+		       return serverutil.Ok()
+	       }
 
-		if result.IsFolder {
-			zipWriter := zip.NewWriter(c.Writer)
-			defer zipWriter.Close()
-			dirFs := os.DirFS(result.FullPath)
-			err := zipWriter.AddFS(dirFs)
-			if err != nil {
-				c.Status(http.StatusInternalServerError)
-				return serverutil.Ok()
-			}
-			c.Writer.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s.zip", filepath.Base(result.FullPath)))
-			c.Writer.Header().Set("Content-Type", "application/zip")
-		} else {
-			file, err := os.Open(result.FullPath)
-			if err != nil {
-				c.Status(http.StatusNotFound)
-				return serverutil.Ok()
-			}
-			defer file.Close()
+	       if result.IsFolder {
+		       zipWriter := zip.NewWriter(c.Writer)
+		       defer zipWriter.Close()
+		       dirFs := os.DirFS(result.FullPath)
+		       err := zipWriter.AddFS(dirFs)
+		       if err != nil {
+			       c.Status(http.StatusInternalServerError)
+			       return serverutil.Ok()
+		       }
+		       c.Writer.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=%s.zip", filepath.Base(result.FullPath)))
+		       c.Writer.Header().Set("Content-Type", "application/zip")
+	       } else {
+		       file, err := os.Open(result.FullPath)
+		       if err != nil {
+			       c.Status(http.StatusNotFound)
+			       return serverutil.Ok()
+		       }
+		       defer file.Close()
 
-			disposition := "inline"
-			contentType := "application/octet-stream"
-			if result.FileType == cirrusutil.FileTypePDF {
-				disposition = "inline"
-				contentType = "application/pdf"
-			}
-			c.Header("Content-Disposition", fmt.Sprintf("%s; filename=%s", disposition, filepath.Base(result.FullPath)))
-			c.Header("Content-Type", contentType)
-			c.File(result.FullPath)
-		}
-		return serverutil.Ok()
-	},
+		       disposition := "inline"
+		       contentType := "application/octet-stream"
+		       if result.FileType == cirrusutil.FileTypePDF {
+			       disposition = "inline"
+			       contentType = "application/pdf"
+		       }
+		       c.Header("Content-Disposition", fmt.Sprintf("%s; filename=%s", disposition, filepath.Base(result.FullPath)))
+		       c.Header("Content-Type", contentType)
+		       c.File(result.FullPath)
+	       }
+	       return serverutil.Ok()
+       },
 )
