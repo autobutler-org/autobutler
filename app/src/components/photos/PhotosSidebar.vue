9b8587f562400c1427b03adfb96d7f272cef0a97
James Orson <jamesaorson@gmail.com>
2026-01-31 11:06:31 -0800

    573: Hide unused features from users
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- app/src/components/photos/PhotosSidebar.vue
+++ app/src/components/photos/PhotosSidebar.vue
@@ -1,30 +1,19 @@
 <template>
-  <LibrarySidebar :sections="sections" />
+  <!-- TODO: Add back once the feature has more going on -->
+  <!-- <LibrarySidebar :sections="sections" /> -->
+  <template></template>
 </template>
 
 <script lang="ts" setup>
-import LibrarySidebar from '@/components/common/LibrarySidebar.vue';
 import { ref } from 'vue';
 
 const props = defineProps<{ photoCount: number }>();
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const sections = ref([
   {
-    title: 'Library',
-    items: [
-      { label: 'All Photos', count: props.photoCount, active: true },
-      { label: 'Albums', count: 0 },
-      { label: 'Favorites', count: 0 },
-      { label: 'Recently Added', count: 0 },
-    ],
-  },
-  {
-    title: 'Smart Albums',
-    items: [
-      { label: 'Screenshots', count: 0 },
-      { label: 'Videos', count: 0 },
-      { label: 'Selfies', count: 0 },
-    ],
+    title: 'Library (More coming soon)',
+    items: [{ label: 'All Photos', count: props.photoCount, active: true }],
   },
 ]);
 </script>
