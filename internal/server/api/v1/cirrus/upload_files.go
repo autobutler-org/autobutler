d7e97b0e3c40343196ada18400a6059c3ce92995
James Orson <jamesaorson@gmail.com>
2026-02-21 19:03:30 -0800

    fix-add-folder: Fix swagger docs
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- internal/server/api/v1/cirrus/upload_files.go
+++ internal/server/api/v1/cirrus/upload_files.go
@@ -33,7 +33,7 @@ func uploadFiles(c *gin.Context) *serverutil.Response {
 // @Param file formData file true "File to upload"
 // @Success 200 {object} serverutil.Response "OK"
 // @Failure 400 {object} serverutil.Response "Bad Request"
-// @Router /cirrus//upload/{rootDir} [post]
+// @Router /cirrus/upload/{rootDir} [post]
 func uploadFilesNested(c *gin.Context, rootDir string) *serverutil.Response {
 	serial := c.Query("serial")
 	reader, err := c.Request.MultipartReader()
