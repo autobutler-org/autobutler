ba8b2dddc463e6d140f9dd3486a85fc87f7e6899
James Orson <jamesaorson@gmail.com>
2026-03-05 11:57:15 -0800

    add-web: Fix linting errors
    Signed-off-by: James Orson <jamesaorson@gmail.com>

--- lib/utils/web_download_web.dart
+++ lib/utils/web_download_web.dart
@@ -1,17 +1,18 @@
-import 'dart:html' as html;
+import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:web/web.dart' as web;
+
 Future<String?> saveBytesForDownload(Uint8List data, String fileName) async {
-  final blob = html.Blob(<dynamic>[data]);
-  final url = html.Url.createObjectUrlFromBlob(blob);
-  final anchor = html.AnchorElement(href: url)
+  final encoded = base64Encode(data);
+  final anchor = web.HTMLAnchorElement()
+    ..href = 'data:application/octet-stream;base64,$encoded'
     ..download = fileName
     ..style.display = 'none';
 
-  html.document.body?.append(anchor);
+  web.document.body?.append(anchor);
   anchor.click();
   anchor.remove();
-  html.Url.revokeObjectUrl(url);
 
   return fileName;
 }
