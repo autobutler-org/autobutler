5fb21ed94469b9405d1ad34cf1d85c6ba1fda75f
Exo <exokomodo@gmail.com>
2026-04-02 12:11:59 -0700

    Revert "fix: use ButtonStyle to enforce toolbar icon colors in dark mode"
    This reverts commit b44c00b45c16ebaeb7f0d0475a23389aebf1cbc8.

--- lib/pages/document_editor_page.dart
+++ lib/pages/document_editor_page.dart
@@ -421,16 +421,9 @@ class _DocumentEditorPageState extends State<DocumentEditorPage> {
                   iconTheme: QuillIconTheme(
                     iconButtonUnselectedData: IconButtonData(
                       color: theme.colorScheme.onSurface,
-                      style: IconButton.styleFrom(
-                        foregroundColor: theme.colorScheme.onSurface,
-                      ),
                     ),
                     iconButtonSelectedData: IconButtonData(
                       color: theme.colorScheme.onPrimary,
-                      style: IconButton.styleFrom(
-                        foregroundColor: theme.colorScheme.onPrimary,
-                        backgroundColor: theme.colorScheme.primary,
-                      ),
                     ),
                   ),
                 ),
