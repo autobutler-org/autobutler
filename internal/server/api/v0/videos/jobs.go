autobutler-org/quark

feat(#1122): video transcode job queue — migration, worker, API, UI
Closes #1122

New table:
  video_jobs (id, job_type, status, device_serial, input_rel_path,
              output_rel_path, params, progress, error_message,
              created_at, updated_at)

Status values: pending | running | completed | failed | cancelled

  CreateVideoJob, GetVideoJob, UpdateVideoJobStatus,
  ListPendingVideoJobs, ListVideoJobs, CancelVideoJob

Started at server boot as a goroutine (v0_videos.StartWorker).
Polls every 5s for pending jobs. Processes one at a time (avoids
CPU contention on Pi/Armbian). Progress updates via ffmpeg
-progress pipe:2 parsing (out_time_ms field → 0.0–1.0 fraction).

  POST /api/v0/videos/transcode  → { jobId }  (202 Accepted)
  GET  /api/v0/videos/jobs/:id  → videoJobJSON
  GET  /api/v0/videos/jobs      → []videoJobJSON  (50 most recent)

Preset aliases: compatible → h264_720p, small → h264_480p, web → webm_720p

CirrusService: transcodeVideo, getVideoJob, listVideoJobs

VideoViewerPage: 'Convert…' in the popup menu opens a SimpleDialog
  with three preset options (Compatible MP4, Small MP4, Web WebM).
  On confirm: queues the job and shows a snackbar with the job ID.
Signed-off-by: Exo <exokomodo@gmail.com>

internal/server/api/v0/videos/jobs.go
@@ -1,0 +1,107 @@
+package v0_videos
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/autobutler-org/autobutler/internal/db"
+	"github.com/autobutler-org/autobutler/pkg/util/ctxutil"
+	"github.com/autobutler-org/autobutler/pkg/util/deputil"
+	"github.com/autobutler-org/autobutler/pkg/util/serverutil"
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
+
+// videoJobJSON is the API representation of a video_jobs row.
+type videoJobJSON struct {
+	ID            int64   `json:"id"`
+	JobType       string  `json:"jobType"`
+	Status        string  `json:"status"`
+	DeviceSerial  string  `json:"deviceSerial,omitempty"`
+	InputRelPath  string  `json:"inputRelPath"`
+	OutputRelPath string  `json:"outputRelPath,omitempty"`
+	Preset        string  `json:"preset,omitempty"`
+	Progress      float64 `json:"progress"`
+	ErrorMessage  string  `json:"errorMessage,omitempty"`
+	CreatedAt     string  `json:"createdAt"`
+	UpdatedAt     string  `json:"updatedAt"`
+}
+
+func jobToJSON(j db.VideoJob) videoJobJSON {
+	return videoJobJSON{
+		ID:            j.ID,
+		JobType:       j.JobType,
+		Status:        j.Status,
+		DeviceSerial:  j.DeviceSerial,
+		InputRelPath:  j.InputRelPath,
+		OutputRelPath: j.OutputRelPath,
+		Progress:      j.Progress,
+		ErrorMessage:  j.ErrorMessage,
+		CreatedAt:     j.CreatedAt.UTC().Format(time.RFC3339),
+		UpdatedAt:     j.UpdatedAt.UTC().Format(time.RFC3339),
+	}
+}
+
+// getVideoJob godoc
+// @Summary Get a video job by ID
+// @Description Returns current status and progress for a video processing job.
+// @Tags videos
+// @Produce json
+// @Param id path int true "Job ID"
+// @Success 200 {object} videoJobJSON
+// @Failure 404 {object} serverutil.Response "Not Found"
+// @Failure 500 {object} serverutil.Response "Internal Server Error"
+// @Router /videos/jobs/{id} [get]
+func getVideoJobHandler(c *gin.Context) *serverutil.Response {
+	deps, ok := ctxutil.Get[deputil.Dependencies](c, "deps")
+	if !ok {
+		return serverutil.InternalServerError(nil)
+	}
+
+	idStr := c.Param("id")
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		return serverutil.BadRequest(fmt.Errorf("invalid job id: %s", idStr))
+	}
+
+	job, err := deps.Database().Queries.GetVideoJob(c.Request.Context(), id)
+	if errors.Is(err, sql.ErrNoRows) {
+		return serverutil.NotFound(fmt.Errorf("job %d not found", id))
+	}
+	if err != nil {
+		return serverutil.InternalServerError(err)
+	}
+
+	return serverutil.Ok().WithContentType(serverutil.ContentTypeJSON).WithData(jobToJSON(job))
+}
+
+// listVideoJobsHandler godoc
+// @Summary List recent video jobs
+// @Description Returns the 50 most recent video processing jobs, newest first.
+// @Tags videos
+// @Produce json
+// @Success 200 {array} videoJobJSON
+// @Failure 500 {object} serverutil.Response "Internal Server Error"
+// @Router /videos/jobs [get]
+func listVideoJobsHandler(c *gin.Context) *serverutil.Response {
+	deps, ok := ctxutil.Get[deputil.Dependencies](c, "deps")
+	if !ok {
+		return serverutil.InternalServerError(nil)
+	}
+
+	jobs, err := deps.Database().Queries.ListVideoJobs(c.Request.Context())
+	if err != nil {
+		return serverutil.InternalServerError(err)
+	}
+
+	result := make([]videoJobJSON, 0, len(jobs))
+	for _, j := range jobs {
+		result = append(result, jobToJSON(j))
+	}
+	return serverutil.Ok().WithContentType(serverutil.ContentTypeJSON).WithData(result)
+}
+
+var getVideoJobRoute = serverutil.ApiRoute("GET", "/videos/jobs/:id", getVideoJobHandler)
+var listVideoJobsRoute = serverutil.ApiRoute("GET", "/videos/jobs", listVideoJobsHandler)

internal/server/api/v0/videos/transcode.go
@@ -1,0 +1,123 @@
+package v0_videos
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"strings"
+
+	"github.com/autobutler-org/autobutler/internal/db"
+	"github.com/autobutler-org/autobutler/pkg/util/ctxutil"
+	"github.com/autobutler-org/autobutler/pkg/util/deputil"
+	"github.com/autobutler-org/autobutler/pkg/util/serverutil"
+	"github.com/autobutler-org/autobutler/pkg/util/storageutil"
+	"github.com/autobutler-org/autobutler/pkg/util/videoutil"
+	"github.com/gin-gonic/gin"
+)
+
+// transcodeRequest is the POST body for /videos/transcode.
+type transcodeRequest struct {
+	RelPath string `json:"relPath"`
+	Serial  string `json:"serial"`
+	Preset  string `json:"preset"` // "compatible" | "small" | "web" | raw preset name
+}
+
+// transcodeJobResponse is returned on success.
+type transcodeJobResponse struct {
+	JobID int64 `json:"jobId"`
+}
+
+// Friendly preset aliases → videoutil.TranscodePreset.
+var presetAliases = map[string]videoutil.TranscodePreset{
+	"compatible":                      videoutil.PresetH264720p,
+	"small":                           videoutil.PresetH264480p,
+	"web":                             videoutil.PresetWebM720p,
+	string(videoutil.PresetH264480p):  videoutil.PresetH264480p,
+	string(videoutil.PresetH264720p):  videoutil.PresetH264720p,
+	string(videoutil.PresetH2641080p): videoutil.PresetH2641080p,
+	string(videoutil.PresetWebM720p):  videoutil.PresetWebM720p,
+}
+
+// queueTranscode godoc
+// @Summary Queue a video transcode job
+// @Description Queues a background transcode job and returns the job ID. Poll GET /videos/jobs/:id for status.
+// @Tags videos
+// @Accept json
+// @Produce json
+// @Param body body transcodeRequest true "Transcode request"
+// @Success 202 {object} transcodeJobResponse
+// @Failure 400 {object} serverutil.Response "Bad Request"
+// @Failure 404 {object} serverutil.Response "Not Found"
+// @Failure 501 {object} serverutil.Response "Not Implemented — ffmpeg not available"
+// @Failure 500 {object} serverutil.Response "Internal Server Error"
+// @Router /videos/transcode [post]
+func queueTranscode(c *gin.Context) *serverutil.Response {
+	if !videoutil.Available() {
+		return serverutil.NewResponse().
+			WithStatusCode(http.StatusNotImplemented).
+			WithContentType(serverutil.ContentTypeJSON).
+			WithData(gin.H{"error": "ffmpeg is not installed on this device"})
+	}
+
+	deps, ok := ctxutil.Get[deputil.Dependencies](c, "deps")
+	if !ok {
+		return serverutil.InternalServerError(nil)
+	}
+
+	var req transcodeRequest
+	if err := json.NewDecoder(c.Request.Body).Decode(&req); err != nil {
+		return serverutil.BadRequest(fmt.Errorf("invalid request body: %w", err))
+	}
+	if req.RelPath == "" {
+		return serverutil.BadRequest(fmt.Errorf("relPath is required"))
+	}
+
+	preset, ok := presetAliases[req.Preset]
+	if !ok {
+		preset = videoutil.PresetH264720p // default: compatible 720p
+	}
+
+	// Validate the file exists.
+	filesDir, err := storageutil.GetCirrusDir()
+	if err != nil {
+		return serverutil.InternalServerError(err)
+	}
+	if req.Serial != "" {
+		if devices, err := deps.StorageService().GetManagedDevices(); err == nil {
+			for _, d := range devices {
+				if d.UsbInfo != nil && d.UsbInfo.GetSerial() == req.Serial {
+					filesDir = d.CirrusDir
+					break
+				}
+			}
+		}
+	}
+	cleanFilesDir := filepath.Clean(filesDir)
+	fullPath := filepath.Join(cleanFilesDir, req.RelPath)
+	if !strings.HasPrefix(fullPath, cleanFilesDir+string(filepath.Separator)) {
+		return serverutil.BadRequest(fmt.Errorf("invalid relPath"))
+	}
+
+	paramsJSON, _ := json.Marshal(transcodeParams{Preset: string(preset)})
+
+	jobID, err := deps.Database().Queries.CreateVideoJob(c.Request.Context(), db.CreateVideoJobParams{
+		JobType:       "transcode",
+		DeviceSerial:  req.Serial,
+		InputRelPath:  req.RelPath,
+		OutputRelPath: "",
+		Params:        string(paramsJSON),
+	})
+	if err != nil {
+		return serverutil.InternalServerError(fmt.Errorf("create job: %w", err))
+	}
+
+	return serverutil.NewResponse().
+		WithStatusCode(http.StatusAccepted).
+		WithContentType(serverutil.ContentTypeJSON).
+		WithData(transcodeJobResponse{JobID: jobID})
+}
+
+var queueTranscodeRoute = serverutil.ApiRoute(
+	"POST", "/videos/transcode", queueTranscode,
+)

autobutler-org/autobutler

internal/server/api/v0/videos/jobs.go
@@ -1,0 +1,107 @@
+package v0_videos
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/autobutler-org/quark/internal/db"
+	"github.com/autobutler-org/quark/pkg/util/ctxutil"
+	"github.com/autobutler-org/quark/pkg/util/deputil"
+	"github.com/autobutler-org/quark/pkg/util/serverutil"
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
+
+// videoJobJSON is the API representation of a video_jobs row.
+type videoJobJSON struct {
+	ID            int64   `json:"id"`
+	JobType       string  `json:"jobType"`
+	Status        string  `json:"status"`
+	DeviceSerial  string  `json:"deviceSerial,omitempty"`
+	InputRelPath  string  `json:"inputRelPath"`
+	OutputRelPath string  `json:"outputRelPath,omitempty"`
+	Preset        string  `json:"preset,omitempty"`
+	Progress      float64 `json:"progress"`
+	ErrorMessage  string  `json:"errorMessage,omitempty"`
+	CreatedAt     string  `json:"createdAt"`
+	UpdatedAt     string  `json:"updatedAt"`
+}
+
+func jobToJSON(j db.VideoJob) videoJobJSON {
+	return videoJobJSON{
+		ID:            j.ID,
+		JobType:       j.JobType,
+		Status:        j.Status,
+		DeviceSerial:  j.DeviceSerial,
+		InputRelPath:  j.InputRelPath,
+		OutputRelPath: j.OutputRelPath,
+		Progress:      j.Progress,
+		ErrorMessage:  j.ErrorMessage,
+		CreatedAt:     j.CreatedAt.UTC().Format(time.RFC3339),
+		UpdatedAt:     j.UpdatedAt.UTC().Format(time.RFC3339),
+	}
+}
+
+// getVideoJob godoc
+// @Summary Get a video job by ID
+// @Description Returns current status and progress for a video processing job.
+// @Tags videos
+// @Produce json
+// @Param id path int true "Job ID"
+// @Success 200 {object} videoJobJSON
+// @Failure 404 {object} serverutil.Response "Not Found"
+// @Failure 500 {object} serverutil.Response "Internal Server Error"
+// @Router /videos/jobs/{id} [get]
+func getVideoJobHandler(c *gin.Context) *serverutil.Response {
+	deps, ok := ctxutil.Get[deputil.Dependencies](c, "deps")
+	if !ok {
+		return serverutil.InternalServerError(nil)
+	}
+
+	idStr := c.Param("id")
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		return serverutil.BadRequest(fmt.Errorf("invalid job id: %s", idStr))
+	}
+
+	job, err := deps.Database().Queries.GetVideoJob(c.Request.Context(), id)
+	if errors.Is(err, sql.ErrNoRows) {
+		return serverutil.NotFound(fmt.Errorf("job %d not found", id))
+	}
+	if err != nil {
+		return serverutil.InternalServerError(err)
+	}
+
+	return serverutil.Ok().WithContentType(serverutil.ContentTypeJSON).WithData(jobToJSON(job))
+}
+
+// listVideoJobsHandler godoc
+// @Summary List recent video jobs
+// @Description Returns the 50 most recent video processing jobs, newest first.
+// @Tags videos
+// @Produce json
+// @Success 200 {array} videoJobJSON
+// @Failure 500 {object} serverutil.Response "Internal Server Error"
+// @Router /videos/jobs [get]
+func listVideoJobsHandler(c *gin.Context) *serverutil.Response {
+	deps, ok := ctxutil.Get[deputil.Dependencies](c, "deps")
+	if !ok {
+		return serverutil.InternalServerError(nil)
+	}
+
+	jobs, err := deps.Database().Queries.ListVideoJobs(c.Request.Context())
+	if err != nil {
+		return serverutil.InternalServerError(err)
+	}
+
+	result := make([]videoJobJSON, 0, len(jobs))
+	for _, j := range jobs {
+		result = append(result, jobToJSON(j))
+	}
+	return serverutil.Ok().WithContentType(serverutil.ContentTypeJSON).WithData(result)
+}
+
+var getVideoJobRoute = serverutil.ApiRoute("GET", "/videos/jobs/:id", getVideoJobHandler)
+var listVideoJobsRoute = serverutil.ApiRoute("GET", "/videos/jobs", listVideoJobsHandler)

internal/server/api/v0/videos/transcode.go
@@ -1,0 +1,123 @@
+package v0_videos
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"strings"
+
+	"github.com/autobutler-org/quark/internal/db"
+	"github.com/autobutler-org/quark/pkg/util/ctxutil"
+	"github.com/autobutler-org/quark/pkg/util/deputil"
+	"github.com/autobutler-org/quark/pkg/util/serverutil"
+	"github.com/autobutler-org/quark/pkg/util/storageutil"
+	"github.com/autobutler-org/quark/pkg/util/videoutil"
+	"github.com/gin-gonic/gin"
+)
+
+// transcodeRequest is the POST body for /videos/transcode.
+type transcodeRequest struct {
+	RelPath string `json:"relPath"`
+	Serial  string `json:"serial"`
+	Preset  string `json:"preset"` // "compatible" | "small" | "web" | raw preset name
+}
+
+// transcodeJobResponse is returned on success.
+type transcodeJobResponse struct {
+	JobID int64 `json:"jobId"`
+}
+
+// Friendly preset aliases → videoutil.TranscodePreset.
+var presetAliases = map[string]videoutil.TranscodePreset{
+	"compatible":                      videoutil.PresetH264720p,
+	"small":                           videoutil.PresetH264480p,
+	"web":                             videoutil.PresetWebM720p,
+	string(videoutil.PresetH264480p):  videoutil.PresetH264480p,
+	string(videoutil.PresetH264720p):  videoutil.PresetH264720p,
+	string(videoutil.PresetH2641080p): videoutil.PresetH2641080p,
+	string(videoutil.PresetWebM720p):  videoutil.PresetWebM720p,
+}
+
+// queueTranscode godoc
+// @Summary Queue a video transcode job
+// @Description Queues a background transcode job and returns the job ID. Poll GET /videos/jobs/:id for status.
+// @Tags videos
+// @Accept json
+// @Produce json
+// @Param body body transcodeRequest true "Transcode request"
+// @Success 202 {object} transcodeJobResponse
+// @Failure 400 {object} serverutil.Response "Bad Request"
+// @Failure 404 {object} serverutil.Response "Not Found"
+// @Failure 501 {object} serverutil.Response "Not Implemented — ffmpeg not available"
+// @Failure 500 {object} serverutil.Response "Internal Server Error"
+// @Router /videos/transcode [post]
+func queueTranscode(c *gin.Context) *serverutil.Response {
+	if !videoutil.Available() {
+		return serverutil.NewResponse().
+			WithStatusCode(http.StatusNotImplemented).
+			WithContentType(serverutil.ContentTypeJSON).
+			WithData(gin.H{"error": "ffmpeg is not installed on this device"})
+	}
+
+	deps, ok := ctxutil.Get[deputil.Dependencies](c, "deps")
+	if !ok {
+		return serverutil.InternalServerError(nil)
+	}
+
+	var req transcodeRequest
+	if err := json.NewDecoder(c.Request.Body).Decode(&req); err != nil {
+		return serverutil.BadRequest(fmt.Errorf("invalid request body: %w", err))
+	}
+	if req.RelPath == "" {
+		return serverutil.BadRequest(fmt.Errorf("relPath is required"))
+	}
+
+	preset, ok := presetAliases[req.Preset]
+	if !ok {
+		preset = videoutil.PresetH264720p // default: compatible 720p
+	}
+
+	// Validate the file exists.
+	filesDir, err := storageutil.GetCirrusDir()
+	if err != nil {
+		return serverutil.InternalServerError(err)
+	}
+	if req.Serial != "" {
+		if devices, err := deps.StorageService().GetManagedDevices(); err == nil {
+			for _, d := range devices {
+				if d.UsbInfo != nil && d.UsbInfo.GetSerial() == req.Serial {
+					filesDir = d.CirrusDir
+					break
+				}
+			}
+		}
+	}
+	cleanFilesDir := filepath.Clean(filesDir)
+	fullPath := filepath.Join(cleanFilesDir, req.RelPath)
+	if !strings.HasPrefix(fullPath, cleanFilesDir+string(filepath.Separator)) {
+		return serverutil.BadRequest(fmt.Errorf("invalid relPath"))
+	}
+
+	paramsJSON, _ := json.Marshal(transcodeParams{Preset: string(preset)})
+
+	jobID, err := deps.Database().Queries.CreateVideoJob(c.Request.Context(), db.CreateVideoJobParams{
+		JobType:       "transcode",
+		DeviceSerial:  req.Serial,
+		InputRelPath:  req.RelPath,
+		OutputRelPath: "",
+		Params:        string(paramsJSON),
+	})
+	if err != nil {
+		return serverutil.InternalServerError(fmt.Errorf("create job: %w", err))
+	}
+
+	return serverutil.NewResponse().
+		WithStatusCode(http.StatusAccepted).
+		WithContentType(serverutil.ContentTypeJSON).
+		WithData(transcodeJobResponse{JobID: jobID})
+}
+
+var queueTranscodeRoute = serverutil.ApiRoute(
+	"POST", "/videos/transcode", queueTranscode,
+)